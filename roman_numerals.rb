0d0634f28199201ec3534b37fab9c41813e37451
Scott Fulton <sfulton70@yahoo.com>
2017-07-19 21:39:34 -0700

    wrote methods and tests for both modern and old roman numerals

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,61 @@
-def convert_to_roman(arabic_number)
-  # Your code goes here
+def convert_to_roman(arabic_number, modern = false)
+	roman_numeral = ""
+	if arabic_number >= 1000
+		roman_numeral+="M"*(arabic_number/1000)
+		arabic_number = arabic_number % 1000
+	end
+	if arabic_number >= 500
+		if arabic_number >= 900 && modern == true
+			roman_numeral += "CM"
+			arabic_number = arabic_number - 900
+		else
+			roman_numeral+="D"*(arabic_number/500)
+			arabic_number = arabic_number % 500
+		end
+
+		roman_numeral+="D"*(arabic_number/500)
+		arabic_number = arabic_number % 500
+	end
+	if arabic_number >= 100
+		if arabic_number >= 400 && modern == true
+			roman_numeral += "CD"
+			arabic_number = arabic_number - 400
+		else
+			roman_numeral+="C"*(arabic_number/100)
+			arabic_number = arabic_number % 100
+		end
+	end
+	if arabic_number >= 50
+		if arabic_number >= 90 && modern == true
+			roman_numeral += "XC"
+			arabic_number = arabic_number - 90
+		else
+			roman_numeral+="L"*(arabic_number/50)
+			arabic_number = arabic_number % 50
+		end
+	end
+	if arabic_number >= 10
+		if arabic_number >= 40 && modern == true
+			roman_numeral += "XL"
+			arabic_number = arabic_number - 40
+		else
+			roman_numeral+="X"*(arabic_number/10)
+			arabic_number = arabic_number % 10
+		end
+	end
+	if arabic_number >= 5
+		if arabic_number == 9 && modern == true
+			roman_numeral += "IX"
+			arabic_number = arabic_number - 9
+		else
+			roman_numeral+="V"*(arabic_number/5)
+			arabic_number = arabic_number % 5
+		end
+	end
+	if arabic_number == 4 && modern == true
+		roman_numeral += "IV"
+	else
+		roman_numeral += "I"*arabic_number
+	end
+	roman_numeral
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -10,14 +10,62 @@ describe 'converting an Arabic number to a Roman numeral' do
       expect(convert_to_roman(4)).to eq "IIII"
     end
 
-    # Release 1 ...
-    # add tests for old roman numerals here
+    it 'converts 5 to V' do
+      expect(convert_to_roman(5)).to eq "V"
+    end
+
+    it 'converts 10 to X' do
+      expect(convert_to_roman(10)).to eq "X"
+    end
+
+    it 'converts 50 to L' do
+      expect(convert_to_roman(50)).to eq "L"
+    end
+
+    it 'converts 100 to C' do
+      expect(convert_to_roman(100)).to eq "C"
+    end
+
+    it 'converts 500 to D' do
+      expect(convert_to_roman(500)).to eq "D"
+    end
+
+    it 'converts 1000 to M' do
+      expect(convert_to_roman(1000)).to eq "M"
+    end
 
   end
 
   describe 'modern Roman numerals' do
-    # Release 3 ...
-    # add tests for modern roman numerals here
+    it 'converts 4 to IV' do
+      expect(convert_to_roman(4, true)).to eq "IV"
+    end
 
+    it 'converts 9 to IX' do
+      expect(convert_to_roman(9, true)).to eq "IX"
+    end
+
+    it 'converts 14 to XIV' do
+      expect(convert_to_roman(14, true)).to eq "XIV"
+    end
+
+    it 'converts 44 to XLIV' do
+      expect(convert_to_roman(44, true)).to eq "XLIV"
+    end
+
+    it 'converts 99 to XCIX' do
+      expect(convert_to_roman(99, true)).to eq "XCIX"
+    end
+
+    it 'converts 400 to CD' do
+      expect(convert_to_roman(400, true)).to eq "CD"
+    end
+
+    it 'converts 944 to CMXLIV' do
+      expect(convert_to_roman(944, true)).to eq "CMXLIV"
+    end
   end
 end
+
+
+
